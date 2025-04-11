a1a2cac9763bd9aa7946361015b310eaa66866ff
logan pommier <79942380+lolo686868@users.noreply.github.com>
2025-04-11 16:34:42 +0200

    Update grapheoriente.h
    Ajout de la fonction trouverComposantesFortementConnexes utilisé avec Tarjan

--- grapheoriente.h
+++ grapheoriente.h
@@ -6,12 +6,17 @@
 
 class GrapheOriente : public graphe {
 private:
-    
+
 
 public:
     GrapheOriente(vector<sommet> tabSommets, vector<arc*> tabArcs);
     std::vector<int> AlgorithmeDuRang(std::vector<int> d_rangs);
     void afficherRangs() const;
+
+
+    void dfsTarjan(int s,const vector<vector<int>>& adj,vector<int>& num,vector<int>& ro,vector<int>& cfc
+                    ,vector<bool>& enPileTarjan,vector<int>& pileTarjan,int& p,int& k)
+    void trouverComposantesFortementConnexes();
 };
 
-#endif // GRAPHEORIENTE_H 
+#endif // GRAPHEORIENTE_H
