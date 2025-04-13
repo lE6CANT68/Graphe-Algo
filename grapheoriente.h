6888b667bd4a41a72764f50649a552c1fd4288cc
lE6CANT68 <98109831+lE6CANT68@users.noreply.github.com>
2025-04-13 15:39:29 +0200

    Update grapheoriente.h
    Ajout de danzig et dijkstra

--- grapheoriente.h
+++ grapheoriente.h
@@ -3,10 +3,12 @@
 
 #include <vector>
 #include "graphe.h"
+#include <climits>  // Pour INT_MAX
 
 class GrapheOriente : public graphe {
 private:
-
+    constexpr static int MAXPOIDS = INT_MAX;
+    constexpr static int INF = INT_MAX;
 
 public:
     GrapheOriente(vector<sommet> tabSommets, vector<arc*> tabArcs);
@@ -17,6 +19,8 @@ public:
     void dfsTarjan(int s,const vector<vector<int>>& adj,vector<int>& num,vector<int>& ro,vector<int>& cfc
                     ,vector<bool>& enPileTarjan,vector<int>& pileTarjan,int& p,int& k)
     void trouverComposantesFortementConnexes();
+    void Dijkstra(int* fs, int* aps, int** p, int s, int*& d, int*& pr);
+    void Dantzig(vector<vector<int>> &L, const vector<vector<int>> &C, int n);
 };
 
 #endif // GRAPHEORIENTE_H
